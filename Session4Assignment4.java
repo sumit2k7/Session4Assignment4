5352e81c6c68210af9908da1f55c4c74c1d9eea8
sumit2k7 <sumit2k7@users.noreply.github.com>
2016-04-22 12:50:07 +0530

    Added files via upload

---
+++ Session4Assignment4.java
@@ -0,0 +1,13 @@
+
+public class Session4Assignment4 {
+
+	public static void main(String[] args) {
+	
+		String  Firstname="Sumit Gautam";
+		System.out.println("String before Reverse is :-> "+Firstname);
+		StringBuffer sb = new StringBuffer(Firstname);
+		System.out.println("String After Reverse is :-> "+ sb.reverse().toString());
+		
+	}
+
+}
